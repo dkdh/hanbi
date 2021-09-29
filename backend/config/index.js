3e51730619415f1279cec3d85d11393b8e2d6cc0
김지환 <bure5kzam@gmail.com>
2021-09-29 09:15:59 +0900

    feat: 웹서버 소켓 연동 완료

---
+++ backend/config/index.js
@@ -0,0 +1,7 @@
+const NODE_ENV = "developments"
+const params = {
+    "developments": {
+        origin: ["http://localhost:8079"]
+    }
+}
+module.exports = params[NODE_ENV]
--- backend/utils/socketio.js
+++ backend/utils/socketio.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-
+const params = require("../config")
 path = "H:\\Project\\Ssafy_2\\PJT2\\sbj3\\S05P21A102\\backend\\assets\\"
 const fd = fs.openSync(path + 'map.txt', 'w+')
 var outF = fs.createWriteStream(null, { flags: 'w', fd });
@@ -9,22 +9,25 @@ mapData = []
 module.exports.createSocket = function (http_server) {
   const io = require("socket.io")(http_server, {
     cors: {
+      origin: params["origin"],
       methods: ["GET", "POST"],
       transports: ["websocket", "polling"],
       credentials: true,
     },
-    allowEIO3: true,
+    // allowEIO3: true,
+    allowEI04: true,
   });
+
 
   const roomName = 'team'
   io.on("connection", async (socket) => {
     socket.join(roomName)
     console.log("Connect from Client: ");
-    // for (i = 0; i < 10; i++) {
+
     socket.on("setMap2Server", async (data) => {
       mapData = data
       socket.to(roomName).emit('setMap2Web', mapData);
     });
-    // }
+
   });
 };
