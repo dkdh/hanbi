5cd93c576a00b7882a52759f3d29fcf9f720e134
김지환 <bure5kzam@gmail.com>
2021-10-08 08:37:43 +0900

    feat: 분실물 수거 추가

--- frontend/src/components/Main/LostCont.vue
+++ frontend/src/components/Main/LostCont.vue
@@ -1,10 +1,53 @@
 <template>
-  <div>
-  </div>
+  <el-card id="LostCont" shadow="always" :body-style="{ padding: '20px' }">
+    <div id="lostWrap" style="width: 100%">
+      <div>
+        <el-card
+          :body-style="{ padding: '0px' }"
+          v-for="e in elements"
+          :key="e"
+        >
+          <img :src="e.imgSrc" class="image" />
+          <div style="padding: 14px">
+            <span>{{ e.header }}</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ e.date }}</time>
+              <!-- <el-button type="text" class="button">Operating</el-button> -->
+            </div>
+          </div>
+        </el-card>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script>
+import "@/assets/css_kjh/LostCont.css";
+
 export default {
+  data: function () {
+    return {
+      elements: [
+        {
+          header: "Key",
+          date: new Date(),
+          imgSrc: require("@/assets/images/key.png"),
+        },
+        {
+          header: "Wallet",
+          date: new Date(),
+          imgSrc: require("@/assets/images/wallet.png"),
+        },
+        {
+          header: "Bag",
+          date: new Date(),
+          imgSrc: require("@/assets/images/bag.png"),
+        },
+      ],
+    };
+  },
+  methods: {},
+  created() {},
 };
 </script>
 <style>
