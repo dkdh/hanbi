6c74bf2d2f04bd43535cca3faed6885784f03925
류한길 <qer7391@naver.com>
2021-10-08 11:32:38 +0900

    fix: launch 파일이 올바른 파일을 실행하도록 수정

--- src/hanbi_mapping/launch/launch.py
+++ src/hanbi_mapping/launch/launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='hanbi_mapping',
+            package='sub3',
             node_executable='client',
             node_name='client'
         ),
