8caa8a35778f36b3af400bdc8ca65f3e173436fb
류한길 <qer7391@naver.com>
2021-10-09 19:59:31 +0900

    feat: 메인 페이지 접근 전 비밀번호 입력 요구 기능

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h2>비밀번호를 입력해주세요</h2>
+    <input type="password" v-model="password">
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      if(this.password == '0310') {
+        sessionStorage.setItem('password', this.password);
+        this.$router.push({ name: 'Main' });
+      }
+    }
+  },
+}
+</script>
