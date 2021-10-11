5c85f121b545283cd1fe3714ad7b7ae81c09d18d
류한길 <qer7391@naver.com>
2021-10-12 00:35:35 +0900

    fix: 상황판 로그 이벤트 vuex 로그 갱신되도록 수정

--- frontend/src/store/Log.js
+++ frontend/src/store/Log.js
@@ -2,9 +2,9 @@ export default {
     namespaced: true,
     state: {
         log: [
-            { timestamp: String(new Date()), content: "vuex" , emergency : 0, pose :{x:0,y:50}},
-            { timestamp: String(new Date()), content: "vuex2", emergency : 0, pose :{x:0,y:50} },
-            { timestamp: String(new Date()), content: "vuex3", emergency : 1, pose :{x:0,y:50} }
+            { timestamp: String(new Date()), content: "vuex" , emergency : 0, pose : (0, 50)},
+            { timestamp: String(new Date()), content: "vuex2", emergency : 0, pose : (50, 50)},
+            { timestamp: String(new Date()), content: "vuex3", emergency : 1, pose : (0, 150)}
         ], 
         num : {
             "robot" : 1,
@@ -13,10 +13,13 @@ export default {
         }
     },
     mutations: {
-        
+        setLog(state, data) {
+            state.log = data;
+            console.log(state.log);
+        }
     },
     actions: {
-        setLog({ state, dispatch}, data) {
+        setLog({ state, dispatch, commit }, data) {
             // vuex log 데이터를 갱신하는 함수
             
             // 추가된 log가 없으면 반환
@@ -26,13 +29,13 @@ export default {
             state.num["emergency"] = 0
             state.num["event"] = 0
 
-            for(let i=0; i <  data.length;i++) {
+            for(let i=0; i < data.length;i++) {
                 if(data[i].emergency == 1) state.num["emergency"] =+1
                 else state.num["event"]+=1
             }
 
-            //vuex 데이터 생신
-            state.log = data
+            //vuex 데이터 갱신
+            commit('setLog', data);
 
             //Map log 랜더링
             dispatch("Map/renderLog", data,{root:true})
